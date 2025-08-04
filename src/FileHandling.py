4483bc2a006909ae406082e97f73c0b879050197
yitzchakdamen <yitzchakdamen@gmail.com>
2025-08-04 11:33:23 +0300

    Finishing work on Uploading-and-saving-files

--- src/FileHandling.py
+++ src/FileHandling.py
@@ -5,15 +5,25 @@ import os
 class FileHandling:
     
     def __init__(self, source_file_directory:str, destination_file_directory:str) -> None:
-        pass
+        self.source_file_directory = source_file_directory
+        self.destination_file_directory = destination_file_directory
     
     def upload_data_file(self, file_name: str) -> pd.DataFrame:
-        pass 
+        os.makedirs(self.source_file_directory, exist_ok=True)
+        file = os.path.join(self.source_file_directory, file_name)
+        return pd.read_csv(file)
     
     def saving_data_file(self, file_name: str, data:pd.DataFrame) -> None:
-        pass
-    
+        os.makedirs(self.destination_file_directory, exist_ok=True)
+        file = os.path.join(self.destination_file_directory, f"{file_name}.csv")
+        data.to_csv(file, index=False)
+            
     def saving_results_json(self, file_name: str, results:dict) -> None:
-        os.makedirs(dir_fils, exist_ok=True)
-        with open(file= os.path.join(dir_fils, f"{file_name}.json") , mode="w", encoding="utf-8") as file:
-            json.dump(obj=model, fp=file, indent=4)
+        os.makedirs(self.destination_file_directory, exist_ok=True)
+        
+        with open(file= os.path.join(self.destination_file_directory, f"{file_name}.json") , mode="w", encoding="utf-8") as file:
+            json.dump(obj=results, fp=file, indent=4)
+
+
+
+    
